ca6c54b257c86518946369157977921e1cfb1659
Eli Lemons <me@elilemons.com>
2025-10-07 15:07:37 -0400

    Getting theme from the right place

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -7,6 +7,7 @@ import React, { useEffect, useState } from 'react'
 import type { Header } from '@/payload-types'
 
 import { Logo } from '@/components/Logo/Logo'
+import { useTheme } from '@/providers/Theme'
 import { HeaderNav } from './Nav'
 
 interface HeaderClientProps {
@@ -17,6 +18,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   /* Storing the value in a useState to avoid hydration errors */
   const [theme, setTheme] = useState<string | null>(null)
   const { headerTheme, setHeaderTheme } = useHeaderTheme()
+  const { theme: themeFromProvider } = useTheme()
   const pathname = usePathname()
 
   useEffect(() => {
@@ -33,7 +35,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-8 flex justify-between">
         <Link href="/">
-          <Logo loading="eager" priority="high" dark={theme === 'dark'} />
+          <Logo loading="eager" priority="high" dark={themeFromProvider === 'dark'} />
         </Link>
         <HeaderNav data={data} />
       </div>
